abb74bf88d5dcddaf096d875611452bb1cdf31a2
nicolas <nicolas@azari.fr>
2017-01-29 02:30:54 +0100

    Update modules

--- test/index.js
+++ test/index.js
@@ -1,2 +1,13 @@
-const test = require('../lib/').isNpmPackage('/home/nico/dev/azaritech/david-cli/package.json');
-console.log(test);
+const path = '/home/nico/dev/azaritech/david-cli/package.json';
+//const path = '../../package.json';
+const test = require('../lib');
+
+test.isFileExist(path, function (valid, message) {
+  if (!valid) {
+    console.log(valid);
+    console.log(message);
+  }
+  if (valid) {
+    console.log(valid);
+  }
+});
